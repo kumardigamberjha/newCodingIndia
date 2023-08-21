9465c7ea5559fd05a830ea76103413cc882fd63c
Digamber Jha <kumardigamberjha@gmail.com>
2023-08-21 23:29:08 +0530

    Working on updations

--- CodingIndia/blogs/views.py
+++ CodingIndia/blogs/views.py
@@ -35,36 +35,9 @@ def addblog(request):
     return render(request, 'blogs/addblog.html', context)
 
 
-from django.http import FileResponse, Http404
-from django.http import HttpResponse
-from django.conf import settings
-import os
-
-@cache_page(60 * 15)
-def download_image(request, image_path):
-    full_path = os.path.join(settings.MEDIA_ROOT, image_path)
-    if os.path.exists(full_path):
-        with open(full_path, 'rb') as image_file:
-            response = FileResponse(image_file)
-            response['Content-Disposition'] = f'attachment; filename="{os.path.basename(full_path)}"'
-            return response
-    else:
-        raise Http404("Image not found")
-    
-
 @cache_page(60 * 15)
 def Readblog(request, post_id):
     blogs = AddBlog.objects.get(pk = post_id)
-    img_url = blogs.img.url
-    
-    print("Img Url: ", img_url)
-    
-    try:
-        response = download_image(request, img_url)
-        return response
-    except Http404 as e:
-        # Handle the case when the image is not found
-        return HttpResponse(str(e), status=404)
     
     read = {'blogs':blogs}
     return render(request, 'blogs/readblogs.html', read)
