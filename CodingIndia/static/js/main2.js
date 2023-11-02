66003673f4f4511259a59f3a2dee2c59da97c07d
Digamber Jha <kumardigamberjha@gmail.com>
2023-11-02 23:43:34 +0530

    Changes Image field to Url field

---
+++ CodingIndia/static/js/main2.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+  const spans = document.querySelectorAll('.loader .m span');
+
+  const colors = ['#ff9933', '#ffffff', '#008000']; // Orange, White, Green
+  const duration = 2000; // Animation duration for each color
+
+  let currentIndex = 0;
+
+  anime({
+    targets: spans,
+    color: function() {
+      return colors[currentIndex];
+    },
+    duration: duration,
+    delay: function(el, i) {
+      return i * duration;
+    },
+    loop: true,
+    update: function(anim) {
+      currentIndex = anim.currentTime % colors.length;
+    }
+  });
+});
+
+
+
+// #### Section ####
+
+const parallax = document.getElementById("parallax");
+
+// Parallax Effect for DIV 1
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.7 + "px";
+  // DIV 1 background will move slower than other elements on scroll.
+});
+
+// JavaScript to toggle sidebar
+const menuButton = document.getElementById('menuButton');
+const closeButton = document.getElementById('closeButton');
+const sidebar = document.getElementById('sidebar');
+
+menuButton.addEventListener('click', () => {
+  sidebar.style.right = '0';
+  closeButton.style.right = '30px';
+});
+
+closeButton.addEventListener('click', () => {
+  sidebar.style.right = '-250px';
+  closeButton.style.right = '-220px';
+});
