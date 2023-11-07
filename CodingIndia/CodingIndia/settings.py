eb38e40d87de805c266a722da73f071d2bf8f834
Digamber Jha <kumardigamberjha@gmail.com>
2023-11-08 00:06:49 +0530

    Working on Data

--- CodingIndia/CodingIndia/settings.py
+++ CodingIndia/CodingIndia/settings.py
@@ -91,9 +91,9 @@ WSGI_APPLICATION = 'CodingIndia.wsgi.application'
 DATABASES = {  
    'default': {  
        'ENGINE': 'django.db.backends.mysql',  
-       'NAME': 'coding',  
+       'NAME': 'codingindia',  
        'USER': 'root',  
-       'PASSWORD': 'someone@123',  
+       'PASSWORD': 'dj@jha1011',  
        'HOST': '127.0.0.1',  
        'PORT': '3306',  
    }  
